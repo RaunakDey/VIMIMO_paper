d15924ae09b57157d699dddca496747404b398a3
RaunakDey <deyraunak111@gmail.com>
2024-07-23 12:27:33 -0400

    SI posterior seivd

--- figures_scripts/SI_figures/SI_SEIVD_post_bayesian.m
+++ figures_scripts/SI_figures/SI_SEIVD_post_bayesian.m
@@ -251,3 +251,176 @@ xlabel(han,'steps');
 %title(han,'yourTitle');
 set(gca,'FontSize',18)
 
+
+
+fig5 = figure;
+ 
+for i = 1:5
+subplot(3,2,i)
+plot(chain_1(transient_id:end,i+32), 'Color',color1 );
+xlim([0 3000])
+hold on;
+end
+
+
+for i = 1:5
+subplot(3,2,i)
+plot(chain_2(transient_id:end,i+32),'Color',color2);
+ylim([  min(min(chain_1(transient_id:end,i+32)),min(chain_2(transient_id:end,i+32))),  max(max(chain_1(transient_id:end,i+32)),max(chain_2(transient_id:end,i+32))) ]);
+xlim([0 3000])
+make_title_host(i);
+end
+
+han=axes(fig5,'visible','off'); 
+%han.Title.Visible='on';
+han.XLabel.Visible='on';
+han.YLabel.Visible='on';
+ylabel(han,'log_{10}D_c (dead cells/mL)');
+xlabel(han,'steps');
+%title(han,'yourTitle');
+set(gca,'FontSize',18)
+
+
+%% autocorr
+
+
+skip  = 10 ; % from chain thinning.
+% if not in use set to 1.
+limit = 0.2;
+
+
+
+
+load("v25.mat",'chain');
+chain_2 = chain(20000:skip:end,:);
+
+fs = 22;
+
+
+
+fig6 = figure;
+
+
+for i = 1:9
+    subplot(5,2,i)
+[acf,lags] = autocorr(chain_2(:,i),NumLags=200);
+bh = bar(acf);
+bh.FaceColor = [211,23,24]./255;
+yline(limit,'--',Color='k',LineWidth=2)
+yline(-limit,'--',Color='k',LineWidth=2)
+
+ylabel('Autocorrelation')
+make_title(i);
+set(gca,'FontSize',fs)
+end
+
+han=axes(fig6,'visible','off'); 
+%han.Title.Visible='on';
+han.XLabel.Visible='on';
+han.YLabel.Visible='on';
+%ylabel(han,'\beta (virions/cell)');
+title(han,'For \beta (virions/cell)');
+set(gca,'FontSize',fs)
+
+
+
+fig7 = figure;
+
+
+for i = 1:9
+    subplot(5,2,i)
+[acf,lags] = autocorr(chain_2(:,i+9),NumLags=200);
+bh = bar(acf);
+bh.FaceColor = [15,104,82]./255;
+
+
+yline(limit,'--',Color='k',LineWidth=2)
+yline(-limit,'--',Color='k',LineWidth=2)
+
+ylabel('Autocorrelation')
+make_title(i);
+set(gca,'FontSize',fs)
+end
+
+han=axes(fig7,'visible','off'); 
+%han.Title.Visible='on';
+han.XLabel.Visible='on';
+han.YLabel.Visible='on';
+%ylabel(han,'\beta (virions/cell)');
+set(gca,'FontSize',fs)
+
+
+fig8 = figure;
+
+
+for i = 1:9
+    subplot(5,2,i)
+[acf,lags] = autocorr(chain_2(:,i+18),NumLags=200);
+bh = bar(acf);
+bh.FaceColor = [211,119,46]./255;
+
+
+yline(limit,'--',Color='k',LineWidth=2)
+yline(-limit,'--',Color='k',LineWidth=2)
+ylabel('Autocorrelation')
+make_title(i);
+set(gca,'FontSize',fs)
+end
+
+han=axes(fig8,'visible','off'); 
+%han.Title.Visible='on';
+han.XLabel.Visible='on';
+han.YLabel.Visible='on';
+%ylabel(han,'\beta (virions/cell)');
+set(gca,'FontSize',fs)
+
+
+
+fig9 = figure;
+
+
+for i = 1:5
+    subplot(5,2,2*i-1)
+[acf,lags] = autocorr(chain_2(:,i+27),NumLags=200);
+bh = bar(acf);
+bh.FaceColor = [62,137,168]./255;
+
+
+yline(limit,'--',Color='k',LineWidth=2)
+yline(-limit,'--',Color='k',LineWidth=2)
+ylabel('Autocorrelation')
+make_title(i);
+set(gca,'FontSize',fs)
+end
+
+
+
+for i = 1:5
+    subplot(5,2,2*i)
+[acf,lags] = autocorr(chain_2(:,i+32),NumLags=200);
+bh = bar(acf);
+bh.FaceColor = [217,76,33]./255;
+
+
+yline(limit,'--',Color='k',LineWidth=2)
+yline(-limit,'--',Color='k',LineWidth=2)
+ylabel('Autocorrelation')
+make_title(i);
+set(gca,'FontSize',fs)
+end
+
+
+
+
+
+han=axes(fig9,'visible','off'); 
+%han.Title.Visible='on';
+han.XLabel.Visible='on';
+han.YLabel.Visible='on';
+%ylabel(han,'\beta (virions/cell)');
+set(gca,'FontSize',fs)
+
+
+
+
+
