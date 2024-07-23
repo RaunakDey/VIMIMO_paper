7a80ed63b58a227723c05638de6d1bd693e28cba
RaunakDey <deyraunak111@gmail.com>
2024-07-23 18:22:09 -0400

    SI posterior seivd

--- figures_scripts/SI_figures/SI_SEIVD_post_bayesian.m
+++ figures_scripts/SI_figures/SI_SEIVD_post_bayesian.m
@@ -422,5 +422,148 @@ set(gca,'FontSize',fs)
 
 
 
+%% convergence stats.
+
+load('./../../community/results/v25.mat');
+chain_1 = chain(10001:10:30000,:);
+chain_2 = chain(30001:10:50000,:);
+
+mean_chain_1 = mean(chain_1);
+mean_chain_2 = mean(chain_2);
+N=length(chain_1);
+M=2;
+
+grand_mean = (mean_chain_1 + mean_chain_2 )/2;
+
+%chain_1 and chain_2 are the chains.
+
+B = N/(M-1) * ((mean_chain_1 - grand_mean).^2 + (mean_chain_2 - grand_mean).^2) ;
+
+v_1 = (std(chain_1)).^2 ;
+v_2 = (std(chain_2)).^2 ;
+
+W = (1/M) * (v_1 + v_2) ;
+R_1 =  ( (N-1)/N * W + (B/N) ) ./W;
+
+
+
+load('./../../community/results/v23.mat');
+chain_1 = chain(30001:10:40000,:);
+chain_2 = chain(40001:10:50000,:);
+
+mean_chain_1 = mean(chain_1);
+mean_chain_2 = mean(chain_2);
+N=length(chain_1);
+M=2;
+
+grand_mean = (mean_chain_1 + mean_chain_2 )/2;
+
+%chain_1 and chain_2 are the chains.
+
+B = N/(M-1) * ((mean_chain_1 - grand_mean).^2 + (mean_chain_2 - grand_mean).^2) ;
+
+v_1 = (std(chain_1)).^2 ;
+v_2 = (std(chain_2)).^2 ;
+
+W = (1/M) * (v_1 + v_2) ;
+R_2 =  ( (N-1)/N * W + (B/N) ) ./W;
+
+
+R =  min(R_1,R_2);
+
+
+b = bar(R_2,'FaceColor','flat');
+
+for i = 1:9
+b.CData(i,:) = [211,23,24]./255;
+end
+
+for i = 10:18
+b.CData(i,:) = [15,104,82]./255;
+end
+
+for i = 19:27
+b.CData(i,:) = [211,119,46]./255;
+end
+
+for i=28:32
+b.CData(i,:) = [62,137,168]./255;
+end
+
+for i=33:37
+b.CData(i,:) = [217,76,33]./255;
+end
+
+
+set(gca,'XTickLabel','');
+hold on;
+line([0.2, 38], [1.1, 1.1],'Color', [0.1,0.1,0.1],'LineStyle','--',LineWidth=2  );
+set(gca,'FontSize',20);
+xlim([0 38])
+ylabel('R_{GR}')
+yticks([0 0.2 0.4 0.6 0.8 0.9 1]);
+
+
+set(gca, 'XTick', [5 14 23 30 35]);
+set(gca, 'XTickLabel', { '\beta_{ij} ' '\phi_{ij}' '\tau_{ij}' 'r_i' '{Dc}_i'});
+set(gca, 'fontname','times');
+
+
+%% geeweke part
+load('./../../community/results/v23.mat');
+chainstats1 = chainstats(chain,mcmcresults);
+geweke1 = chainstats1(:,5);
+
+load('./../../community/results/v25.mat');
+chainstats2 = chainstats(chain,mcmcresults);
+geweke2 = chainstats2(:,5);
+
+
+geweke_stats = max(geweke1,geweke2);
+
+b = bar(geweke_stats,'FaceColor','flat');
+
+for i = 1:9
+b.CData(i,:) = [211,23,24]./255;
+end
+
+for i = 10:18
+b.CData(i,:) = [15,104,82]./255;
+end
+
+for i = 19:27
+b.CData(i,:) = [211,119,46]./255;
+end
+
+for i=28:32
+b.CData(i,:) = [62,137,168]./255;
+end
+
+for i=33:37
+b.CData(i,:) = [217,76,33]./255;
+end
+
+
+set(gca,'XTickLabel','');
+hold on;
+line([0.2, 38], [0.9, 0.9],'Color', [0.1,0.1,0.1],'LineStyle','--',LineWidth=2  );
+set(gca,'FontSize',20);
+xlim([0 38])
+ylabel('R_{geweke}')
+yticks([0 0.2 0.4 0.6 0.8 0.9 1]);
+
+
+set(gca, 'XTick', [5 14 23 30 35]);
+set(gca, 'XTickLabel', { '\beta_{ij} ' '\phi_{ij}' '\tau_{ij}' 'r_i' '{Dc}_i'});
+set(gca, 'fontname','times');
+
+
+%% covariance plots.
+
+correlation_pearson = corrcoef(chain(30000:end,:));
+imagesc(correlation_pearson);
+
+% this will not be used in the paper and I will only revisit them if I have
+% to.
 
 
